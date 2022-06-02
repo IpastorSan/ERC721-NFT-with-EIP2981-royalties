1d39aba1a6af2f5b067419cc7169c621c540caeb
IpastorSan <ignaciopastorsan@gmail.com>
2022-06-02 17:19:30 +0200

    deploy-script, markdown, interface and contract

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# nftLandingSmartContract
- nftLandingSmartContract
+# Basic NFT Contract with EIP-2981 Royalties
+
+This is a basic NFT contract that you could find in the wild. I kept it minimal yet useful. It is designed with a 10l pfp project in mind.
+
+Features:
+- EIP-2981 royalties. Set to send 5% of sales proceeds to the deployer of the contract. Note that even if I am using (owner()) in the constructor, this is a piece of information that needs to be overwritten calling ````setRoyalties```` if the contract change Owner.
+- ````openPublicSale()```` to grant the deployer more control on the minting process, it needs to be called for the minting to happen.
+- Max-tokens-per-wallet and max-tokens-per-mint. Avoid that any single wallet hoards all your collection. This can be bypassed spunning new wallets, but its annoying and the minter has to pay gas fees repeatedly.
+- Modifier to only allow calls from EOD, not from other smart contracts. This is used to avoid certain exploits.
+- ````reveal()```` function to change the baseTokenUri and improve the fairness of minting.
+- ````withdraw()```` function. I said this is a minimal yet useful example. You dont want your funds to be locked in the smart contract forever, right? However, bear in mind that this only allows you to withdraw ETH, if any ERC20 is sent to the contract, it will be locked.
+
+Also, I have included some basic Waffle tests to the project and a gas report from [hardhat-gas-reporter](https://www.npmjs.com/package/hardhat-gas-reporter)
+
+## Useful commands to run the project 
+
+You need to have Node.js (>=12.0)installed in your computer
+You can find it [here](https://nodejs.org/en/)
+
+## Install project dependencies
+```bash
+npm install
+```
+
+## Install dotenv to store environment variables and keep them secret
+
+You need at least these variables in your .env file. BE SURE TO ADD IT TO GITIGNORE
+
+*This is not compulsory, you could use public RPC URL, but Alchemys work really well and their free tier is more than enough (not sponsored)*
+DEVELOPMENT_ALCHEMY_KEY = "somestringhere"
+PRODUCTION_ALCHEMY_KEY = "somestringhere"
+
+PRIVATE_KEY_DEVELOPMENT = "somenumberhere"
+PRIVATE_KEY_PRODUCTION = "somenumberhere"
+
+*To verify the contracts on Etherscan/polyscan etc*
+ETHERSCAN_KEY = "anothernumberhere"
+
+The following are optional and less security critical
+
+PUBLIC_ADDRESS = "notsosecretstringofnumbers"
+
+# Use the project
+## deploy contract 
+run with this for testing: 
+```bash
+npx hardhat run scripts/deploy-script.js --network rinkeby 
+```
+run with this for mainnet: 
+```bash
+npx hardhat run scripts/deploy-script.js --network mainnet
+```
+
+# Run tests
+```bash
+npx hardhat test test/test.js 
+```
+
+## Veryfy contract 
+```bash
+npx hardhat verify --network rinkeby **contractAddress**
+```
+
---
+++ contracts/NFTContract.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import './interfaces/ERC2981Interface.sol';
+
+contract NFTContract is ERC721, Ownable, IERC2981Royalties {
+    using Counters for Counters.Counter;
+
+    struct RoyaltyInfo {
+        address recipient;
+        uint256 amount;
+    }
+    
+    RoyaltyInfo private _royalties;
+
+    //Interface for royalties
+    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+
+    Counters.Counter private _tokenIds;
+
+    bool private publicSaleIsOpen = false;
+
+    //comparisons are strictly less than for gas efficiency.
+    uint256 public constant MAX_SUPPLY = 10000;
+
+    
+    uint256 public constant PRICE = 0.1 ether;
+
+    uint256 public constant MAX_PER_MINT = 6; //5
+    uint256 public constant MAX_PER_WALLET = 6; //5
+
+    uint96 public constant ROYALTIES_POINTS = 500; //5%
+
+    string public baseTokenURI;
+
+    event NFTMinted(uint256, uint256, address);
+
+    //amount of mints that each address has executed.
+    mapping(address => uint256) public mintsPerAddress;
+
+    constructor(string memory baseURI) ERC721("NFTContract", "NFT") {
+        baseTokenURI = baseURI;
+        setRoyalties(owner(), ROYALTIES_POINTS);
+        
+    }
+
+    //ensure that modified function cannot be called by another contract
+    modifier callerIsUser() {
+    require(tx.origin == msg.sender, "The caller is another contract");
+    _;
+  }
+
+    function _baseURI() internal view override returns (string memory) {
+       return baseTokenURI;
+    }
+    
+    function reveal(string memory _newBaseTokenURI) public onlyOwner {
+        baseTokenURI = _newBaseTokenURI;
+    }
+
+    function openPublicSale() external onlyOwner {
+        require(publicSaleIsOpen == false, 'Sale is already Open!');
+        publicSaleIsOpen = true;
+    }
+
+    function mintNFTs(uint256 _number) public callerIsUser payable {
+        uint256 totalMinted = _tokenIds.current();
+
+        require(publicSaleIsOpen == true, "Opensale is not Open");
+        require(totalMinted + _number < MAX_SUPPLY, "Not enough NFTs!");
+        require(mintsPerAddress[msg.sender] + _number < MAX_PER_WALLET, "Cannot mint more than 5 NFTs per wallet");
+        require(_number > 0 && _number < MAX_PER_MINT, "Cannot mint specified number of NFTs.");
+        require(msg.value == PRICE * _number , "Not enough/too much ether sent");
+        
+        mintsPerAddress[msg.sender] += _number;
+
+        for (uint i = 0; i < _number; ++i) {
+            _mintSingleNFT();
+        }
+
+        emit NFTMinted(_number, _tokenIds.current(), msg.sender);
+    }
+
+
+    function _mintSingleNFT() internal {
+      uint newTokenID = _tokenIds.current();
+      _safeMint(msg.sender, newTokenID);
+      _tokenIds.increment();
+
+    }
+
+    function getCurrentId() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+//Withdraw money in contract to Owner
+    function withdraw() external onlyOwner {
+     uint256 balance = address(this).balance;
+     require(balance > 0, "No ether left to withdraw");
+
+     (bool success, ) = payable(owner()).call{value: balance}("");
+
+     require(success, "Transfer failed.");
+     
+    }
+
+    //interface for royalties
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721) returns (bool){
+
+        return interfaceId == type(IERC2981Royalties).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    function setRoyalties(address recipient, uint256 value) public onlyOwner {
+        require(value <= 10000, 'ERC2981Royalties: Too high');
+
+        _royalties = RoyaltyInfo(recipient, value);
+    }
+
+    function royaltyInfo(uint256, uint256 value) external view override returns (address receiver, uint256 royaltyAmount)
+    {
+        RoyaltyInfo memory royalties = _royalties;
+        receiver = royalties.recipient;
+        royaltyAmount = (value * royalties.amount) / 10000;
+    }
+
+        //fallback receive function
+        receive() external payable { 
+    }
+    
+}
